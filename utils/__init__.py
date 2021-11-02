727e8ec6f32789ce03b83f154814eddfd81dc8ce
pooruss <57995076+pooruss@users.noreply.github.com>
2021-11-02 18:12:49 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,4 +3,4 @@ from .loss import ICNetLoss
 from .metric import SegmentationMetric
 from .logger import SetupLogger
 from .visualize import get_color_pallete
-from .utils_lr import MetricLogger, SmoothedValue
+# from .utils_lr import MetricLogger, SmoothedValue
