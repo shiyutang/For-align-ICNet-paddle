6d004b7b9b758962546c9e21d7de5fa5ad330e1e
pooruss <57995076+pooruss@users.noreply.github.com>
2021-11-02 17:05:42 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,14 +1,15 @@
 import os
 import time
+import datetime
 import yaml
+import shutil
 import paddle
 import numpy as np
 
 from PIL import Image
 from paddle.vision import transforms
 from models import ICNet
-from utils import SegmentationMetric, SetupLogger, get_color_pallete
-
+from utils import ICNetLoss, IterationPolyLR, SegmentationMetric, SetupLogger, get_color_pallete
 
 class Evaluator(object):
     def __init__(self, cfg):
@@ -19,12 +20,12 @@ class Evaluator(object):
         self.image_paths, self.mask_paths = _get_city_pairs(cfg["train"]["cityscapes_root"], "val")
 
         # create network
-        self.model = ICNet(nclass=19, backbone='resnet50')
-
+        self.model = ICNet(nclass = 19, backbone='resnet50')
+        
         # load ckpt
         pretrained_net = paddle.load(cfg["test"]["ckpt_path"])
         self.model.load_dict(pretrained_net)
-
+        
         # evaluation metrics
         self.metric = SegmentationMetric(19)
 
@@ -39,56 +40,55 @@ class Evaluator(object):
         list_mIoU = []
 
         for i in range(len(self.image_paths)):
-            image = Image.open(self.image_paths[i]).convert('RGB')  # image shape: (W,H,3)
-            mask = Image.open(self.mask_paths[i])  # mask shape: (W,H)
 
-            image = self._img_transform(image)  # image shape: (3,H,W) [0,1]
-            mask = self._mask_transform(mask)  # mask shape: (H,w)
+            image = Image.open(self.image_paths[i]).convert('RGB') # image shape: (W,H,3)
+            mask = Image.open(self.mask_paths[i])                  # mask shape: (W,H)
+            print("mask:",mask)
+            image = self._img_transform(image)                     # image shape: (3,H,W) [0,1]
+            mask = self._mask_transform(mask)                      # mask shape: (H,w)
 
-            image = paddle.unsqueeze(image, 0)  # image shape: (1,3,H,W) [0,1]
+            image = paddle.unsqueeze(image, 0)                      # image shape: (1,3,H,W) [0,1]
 
             with paddle.no_grad():
                 start_time = time.time()
                 outputs = model(image)
                 end_time = time.time()
-                step_time = end_time - start_time
+                step_time = end_time-start_time
             self.metric.update(outputs[0], mask)
             pixAcc, mIoU = self.metric.get()
             list_time.append(step_time)
-            lsit_pixAcc.append(pixAcc)
-            list_mIoU.append(mIoU)
+            lsit_pixAcc.append(pixAcc.item())
+            list_mIoU.append(mIoU.item())
             logger.info("Sample: {}, validation pixAcc: {}, mIoU: {}, time: {}s".format(
                 i + 1, pixAcc * 100, mIoU * 100, step_time))
-
+            
             filename = os.path.basename(self.image_paths[i])
             prefix = filename.split('.')[0]
 
-            # save pred
+            # save pred 
             pred = paddle.argmax(outputs[0], 1)
             pred = pred.numpy()
             pred = pred.squeeze(0)
             pred = get_color_pallete(pred, "citys")
             pred.save(os.path.join(outdir, prefix + "_mIoU_{}.png".format(mIoU)))
-
+            
             # save image
-            image = Image.open(self.image_paths[i]).convert('RGB')  # image shape: (W,H,3)
+            image = Image.open(self.image_paths[i]).convert('RGB') # image shape: (W,H,3)
             image.save(os.path.join(outdir, prefix + '_src.png'))
-
+            
             # save target
-            mask = Image.open(self.mask_paths[i])  # mask shape: (W,H)
+            mask = Image.open(self.mask_paths[i])                   # mask shape: (W,H)
             mask = self._class_to_index(np.array(mask).astype('int32'))
             mask = get_color_pallete(mask, "citys")
             mask.save(os.path.join(outdir, prefix + '_label.png'))
 
-        average_pixAcc = sum(lsit_pixAcc) / len(lsit_pixAcc)
-        average_mIoU = sum(list_mIoU) / len(list_mIoU)
-        average_time = sum(list_time) / len(list_time)
+        average_pixAcc = sum(lsit_pixAcc)/len(lsit_pixAcc)
+        average_mIoU = sum(list_mIoU)/len(list_mIoU)
+        average_time = sum(list_time)/len(list_time)
         self.current_mIoU = average_mIoU
-        logger.info(
-            "Evaluate: Average mIoU: {}, Average pixAcc: {}, Average time: {}".format(average_mIoU, average_pixAcc,
-                                                                                      average_time))
-
-    def _img_transform(self, image):
+        logger.info("Evaluate: Average mIoU: {}, Average pixAcc: {}, Average time: {}".format(average_mIoU, average_pixAcc, average_time))
+        
+    def _img_transform(self, image):            
         image_transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize([.485, .456, .406], [.229, .224, .225])])
@@ -98,7 +98,7 @@ class Evaluator(object):
     def _mask_transform(self, mask):
         mask = self._class_to_index(np.array(mask).astype('int32'))
         return paddle.Tensor(np.array(mask).astype('int32'))
-
+        
     def _class_to_index(self, mask):
         # assert the value
         values = np.unique(mask)
@@ -115,7 +115,6 @@ class Evaluator(object):
         index = np.digitize(mask.ravel(), self._mapping, right=True)
         # 依据上述索引index，根据_key，得到对应的mask图
         return self._key[index].reshape(mask.shape)
-
 
 def _get_city_pairs(folder, split='train'):
     def get_path_pairs(img_folder, mask_folder):
@@ -155,25 +154,29 @@ def _get_city_pairs(folder, split='train'):
         return img_paths, mask_paths
     # return img_paths, mask_paths
 
-
 if __name__ == '__main__':
     # Set config file
     config_path = "./configs/icnet.yaml"
     with open(config_path, "r") as yaml_file:
         cfg = yaml.load(yaml_file.read())
+        #print(cfg)
+        #print(cfg["model"]["backbone"])
         print(cfg["train"]["specific_gpu_num"])
-
+    
     # Use specific GPU
     os.environ["CUDA_VISIBLE_DEVICES"] = str(cfg["train"]["specific_gpu_num"])
     num_gpus = len(cfg["train"]["specific_gpu_num"].split(','))
+    # print("torch.cuda.is_available(): {}".format(torch.cuda.is_available()))
+    # print("torch.cuda.device_count(): {}".format(torch.cuda.device_count()))
+    # print("torch.cuda.current_device(): {}".format(torch.cuda.current_device()))
 
     outdir = os.path.join(cfg["train"]["ckpt_dir"], "evaluate_output")
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-
-    logger = SetupLogger(name="semantic_segmentation",
-                         save_dir=cfg["train"]["ckpt_dir"],
-                         distributed_rank=0,
+    
+    logger = SetupLogger(name = "semantic_segmentation", 
+                         save_dir = cfg["train"]["ckpt_dir"], 
+                         distributed_rank = 0, 
                          filename='{}_{}_evaluate_log.txt'.format(cfg["model"]["name"], cfg["model"]["backbone"]))
 
     evaluator = Evaluator(cfg)
